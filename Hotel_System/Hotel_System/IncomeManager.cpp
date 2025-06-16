defbb96cbbfdda1aa4a1a560aac90ad73fe73c81
Salihe Bodeva <salihe.bodeva10@gmail.com>
2025-06-16 04:42:18 +0300

    added comments for the course

--- Hotel_System/Hotel_System/IncomeManager.cpp
+++ Hotel_System/Hotel_System/IncomeManager.cpp
@@ -1,3 +1,20 @@
+/**
+*
+* Solution to course project #5
+* Object-Oriented Programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Summer semester 2024 / 2025
+*
+* @author Salihe Ali Bodeva
+* @idnumber 9MI0600468
+* @compiler VS
+*
+*
+*/
+
+
+
+
 #include "IncomeManager.h"
 #include "Constants.h"
 
--- Hotel_System/Hotel_System/PricingStrategy.h
+++ Hotel_System/Hotel_System/PricingStrategy.h
@@ -1,3 +1,18 @@
+/**
+*
+* Solution to course project #5
+* Object-Oriented Programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Summer semester 2024 / 2025
+*
+* @author Salihe Ali Bodeva
+* @idnumber 9MI0600468
+* @compiler VS
+*
+*
+*/
+
+
 #pragma once
 
 class PricingStrategy {
